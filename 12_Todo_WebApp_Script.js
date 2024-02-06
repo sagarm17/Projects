750da2906c064367cd01ce5105aa3e49e3709ceb
Sagar <sagarmahindrakar018@gmail.com>
2024-02-05 21:07:21 -0600

    First Commit

---
+++ 12_Todo_WebApp_Script.js
@@ -0,0 +1,36 @@
+const input_box = document.getElementById("input-box");
+const list_container = document.getElementById("list-container");
+function add_note(){
+    if(input_box.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = input_box.value;
+        list_container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    input_box.value = "";
+    saveData(); 
+}
+
+list_container.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();        
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", list_container.innerHTML);
+}
+function showTask(){
+    list_container.innerHTML = localStorage.getItem("data");
+}
+showTask();
